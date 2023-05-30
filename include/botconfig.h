b5f5133be000ff10817459015d9bca38912c0581
TheCodingGuy15 <replytoyashn@gmail.com>
2023-05-30 22:36:10 +0100

    first commit

---
+++ include/botconfig.h
@@ -0,0 +1,81 @@
+#include "pros/imu.hpp"
+#include "pros/motors.hpp"
+#include "main.h"
+
+class smoothDriver {
+    public:
+        smoothDriver(int inertialPort, pros::Motor& leftBack, pros::Motor& rightBack, pros::Motor& leftMid, pros::Motor& rightMid, pros::Motor& leftFront, pros::Motor& rightFront);
+
+        smoothDriver(int inertialPort, pros::Motor& leftBack, pros::Motor& rightBack, pros::Motor& leftFront, pros::Motor& rightFront);
+
+        void drive(double pointX, double pointY, double speedMax);
+        void driveDist(double distance, double speedMax);
+
+        void turnToFace(double pointX, double pointY, double speedMax);
+
+        void turn(int targetAng, double speedMax);
+
+
+        void straightTune();
+
+        void turnTune();
+
+        void resetDrive();
+
+        void setDrivePID(double newP, double newI, double newD);
+        void setTurnPID(double newHP, double newHI, double newHD);
+
+        void setExitConditions(double threshold, double totalTime, double tThreshold);
+
+
+        void setKcurve(double newK);
+        void setAcurve(double newA);
+        void startDriver();
+        double returnPos();
+
+        double returnDist(int x, int y);
+        void runPIDTune(double stepCount);
+        void runPIDTurnTune(double stepCount);
+        
+
+
+
+
+
+
+
+    private:
+
+    pros::MotorGroup m_leftMotorGroup;
+    pros::MotorGroup m_rightMotorGroup;
+    pros::Imu m_imu;
+
+    double kP = 0;
+    double kI = 0;
+    double kD = 0;
+    double driveThreshold = 200;
+
+
+    double turnkP = 0;
+    double turnkI = 0;
+    double turnkD = 0;
+    double turnThreshold = 2;
+
+    void setPower(double pow);
+    void setTurnPower(double pow);
+
+    int curveDrive(int inputVal);
+
+    double posX;
+    double posY;
+
+    double kCurve = 0.1;
+    double aCurve = 0.4;
+    double longTime = 5000;
+
+    
+
+};
+
+
+
