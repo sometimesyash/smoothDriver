dccabd44476616e849ad881e463993d2f3e9b945
yash <57414242+sometimesyash@users.noreply.github.com>
2023-09-11 20:22:38 +0100

    Added Dual Mapping

--- src/odom.cpp
+++ src/odom.cpp
@@ -11,32 +11,97 @@ void smoothDriver::mapperWrapper(void *param){
 
 }
 
+double smoothDriver::getLeftDist(){
+
+    double leftVal;
+    if(encodersIn){
+        leftVal = leftEnc.get_value();
+    } else {
+        leftVal = m_leftMotorGroup[0].get_position();
+    }
+
+
+    return leftVal;
+    
+}
+
+double smoothDriver::getRightDist(){
+
+    double rightVal;
+    if(encodersIn){
+        rightVal = rightEnc.get_value();
+    } else {
+        rightVal = m_rightMotorGroup[0].get_position();
+    }
+
+
+    return rightVal;
+    
+}
+
+double leftDist = 0;
+double rightDist = 0;
+
+
+
 void smoothDriver::map(){
 
 
     double lastHeading = 0.0;
     double lastDistL = 0.0;
     double lastDistR = 0.0;
+    double lastTheta = 0.0;
+    double theta = 0.0;
+    bool Which = false;
+
+    if(inertialIn){ Which = true; };
 
 
 
     while(runMap) {
 
-        double leftEnc = m_leftMotorGroup[0].get_position();
-        double rightEnc = m_rightMotorGroup[0].get_position();
-        double theta = m_imu.get_heading();
+        if(Which){
+            double leftEnc = getLeftDist();
+            double rightEnc = getRightDist();
+            double theta = m_imu.get_heading();
 
-        double changeL = m_leftMotorGroup[0].get_position() - lastDistL;
-        double changeR = m_rightMotorGroup[0].get_position() - lastDistR;
+            double changeL = leftEnc - lastDistL;
+            double changeR =rightEnc - lastDistR;
 
-        double avg = (changeL + changeR)/2;
+            double avg = (changeL + changeR)/2;
+            
+            lastDistL = leftEnc;
+            lastDistR = rightEnc;
+
+            posX += avg * cos(theta);
+            posY += avg * sin(theta);
+
+        } else {
+            double leftEnc = getLeftDist();
+            double rightEnc = getRightDist();
+            double changeL = leftEnc - lastDistL;
+            double changeR = rightEnc - lastDistR;
+            
+
+            double changeTheta = (changeL - changeR)/(leftDist + rightDist);
+            theta += changeTheta;
+
+            double Y = 2*((changeR/changeTheta)+rightDist) * (theta + (changeTheta/2));
+
+            posX+= Y * sin(theta + (changeTheta/2));
+            posY += Y * cos(theta + (changeTheta/2));
+
+
+
+        }
+
+
+
+
         
-        lastDistL = leftEnc;
-        lastDistR = rightEnc;
 
-        
-        posX += avg * cos(theta);
-        posY = avg * sin(theta);
+        pros::screen::print(pros::E_TEXT_MEDIUM, 2, "XPos:%3d", posX);
+        pros::screen::print(pros::E_TEXT_MEDIUM, 4, "Ypos: %3d", posY);
     }
 
 }
